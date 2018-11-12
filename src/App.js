78a333423d54837782b0b04f17e2429dbe00428f
Vodoprovod <naiznanku45@mail.ru>
2018-11-12 23:43:26 +0300

    1

--- src/App.js
+++ src/App.js
@@ -67,15 +67,47 @@ class App extends Component {
     const { searchTerm, list } = this.state;
     return (
       <div className="App">
-          <form>
-              <input
-                  type="text"
-                  value={ searchTerm }
-                  onChange={ this.onSearchChange }
-              />
-          </form>
-          { list.filter(isSearched(searchTerm)).map( item  =>
-                  <div key={ item.objectID }>
+          <Search
+            value = { searchTerm }
+            onChange = { this.onSearchChange }
+          >
+              Поиск:
+          </Search>
+          <Table
+            list = { list }
+            pattern = { searchTerm }
+            onDismiss = { this.onDismiss }
+          />
+      </div>
+    );
+  }
+}
+
+class Search extends Component {
+    render() {
+        const { value, onChange, children } = this.props;
+
+        return (
+            <form>
+                <span>{ children }</span>
+                <input
+                    type="text"
+                    value={ value }
+                    onChange={ onChange }
+                />
+            </form>
+        );
+    }
+}
+
+class Table extends Component {
+    render() {
+        const { list, pattern, onDismiss } = this.props;
+
+        return (
+            <div>
+            { list.filter(isSearched(pattern)).map( item  =>
+                <div key={ item.objectID }>
                     <span>
                       <a href={ item.url }>{ item.title }</a>
                     </span>
@@ -84,17 +116,21 @@ class App extends Component {
                     <span>{ item.points }</span>
                     <span>
                         <button
-                            onClick={() => this.onDismiss(item.objectID)}
+                            onClick={() => onDismiss(item.objectID)}
                             type="button"
                         >
                             Отбросить
                         </button>
                     </span>
-                  </div>
-          ) }
-      </div>
-    );
-  }
+                </div>
+            ) }
+            </div>
+        );
+    }
 }
 
 export default App;
+
+
+
+
