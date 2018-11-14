5b10c857f082dd01795366ded59a6d80e77ae050
Vodoprovod <naiznanku45@mail.ru>
2018-11-15 00:31:08 +0300

    1

--- src/App.js
+++ src/App.js
@@ -2,32 +2,38 @@ import React, { Component } from 'react';
 //import logo from './logo.svg';
 import './App.css';
 
-const list = [
-    {
-        title: 'React',
-        url: 'https://reactjs.org',
-        author: 'Jordan Walke',
-        num_comments: 3,
-        points: 4,
-        objectID: 0
-    },
-    {
-        title: 'Redux',
-        url: 'https://redux.js.org',
-        author: 'Dan Abramov, Andrew Clarck',
-        num_comments: 2,
-        points: 5,
-        objectID: 1
-    },
-    {
-        title: 'Mail.ru',
-        url: 'https://mail.ru',
-        author: 'Syoma',
-        num_comments: 7,
-        points: 8,
-        objectID: 2
-    }
-];
+// const list = [
+//     {
+//         title: 'React',
+//         url: 'https://reactjs.org',
+//         author: 'Jordan Walke',
+//         num_comments: 3,
+//         points: 4,
+//         objectID: 0
+//     },
+//     {
+//         title: 'Redux',
+//         url: 'https://redux.js.org',
+//         author: 'Dan Abramov, Andrew Clarck',
+//         num_comments: 2,
+//         points: 5,
+//         objectID: 1
+//     },
+//     {
+//         title: 'Mail.ru',
+//         url: 'https://mail.ru',
+//         author: 'Syoma',
+//         num_comments: 7,
+//         points: 8,
+//         objectID: 2
+//     }
+// ];
+
+const DEFAULT_QUERY = 'redux';
+
+const PATH_BASE = 'https://hn.algolia.com/api/v1';
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
 
 function isSearched(searchTerm) {
     return function (item) {
@@ -41,21 +47,38 @@ class App extends Component {
       super(props);
 
       this.state = {
-        list,
-        searchTerm: ""
+        result: null,
+        searchTerm: DEFAULT_QUERY
       };
 
+      this.setSearchTopStories = this.setSearchTopStories.bind(this);
       this.onDismiss = this.onDismiss.bind(this);
       this.onSearchChange = this.onSearchChange.bind(this);
+    }
+
+    setSearchTopStories(result) {
+        this.setState({ result });
+    }
+
+    componentDidMount() {
+        const { searchTerm } = this.state;
+
+        fetch(`${ PATH_BASE }${ PATH_SEARCH }?${ PARAM_SEARCH }${ searchTerm }`)
+            .then(response => response.json())
+            .then(result => this.setSearchTopStories(result))
+            .catch(error => error);
     }
 
     onDismiss(id) {
 
         const isNotId = item => item.objectID !== id;
 
-        const updatedList = this.state.list.filter(isNotId);
+        //const updatedList = this.state.list.filter(isNotId);
+        const updatedHits = this.state.result.hits.filter(isNotId);
 
-        this.setState({ list: updatedList });
+        this.setState({
+            result: { ...this.state.result, hits: updatedHits }
+        });
     }
 
     onSearchChange(event) {
@@ -64,7 +87,11 @@ class App extends Component {
 
   render() {
 
-    const { searchTerm, list } = this.state;
+    const { searchTerm, result } = this.state;
+    console.log(this.state);
+
+    //if (!result) { return null }
+
     return (
       <div className="page">
           <div className="interactions">
@@ -75,11 +102,14 @@ class App extends Component {
                   Поиск:
               </Search>
           </div>
-          <Table
-            list = { list }
-            pattern = { searchTerm }
-            onDismiss = { this.onDismiss }
-          />
+          { result ?
+              <Table
+                  list={ result.hits }
+                  pattern={ searchTerm }
+                  onDismiss={ this.onDismiss }
+              />
+              : null
+          }
       </div>
     );
   }
