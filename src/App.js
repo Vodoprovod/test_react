e4dd0a031a7a37ed55bf9a50f30124bb42d808e7
Vodoprovod <naiznanku45@mail.ru>
2018-11-22 01:02:43 +0300

    1

--- src/App.js
+++ src/App.js
@@ -52,7 +52,8 @@ class App extends Component {
       this.state = {
         results: null,
         searchKey: '',
-        searchTerm: DEFAULT_QUERY
+        searchTerm: DEFAULT_QUERY,
+        error: null
       };
 
       this.needsToSearchTopStories = this.needsToSearchTopStories.bind(this);
@@ -93,7 +94,7 @@ class App extends Component {
         fetch(`${ PATH_BASE }${ PATH_SEARCH }?${ PARAM_SEARCH }${ searchTerm }&${ PARAM_PAGE }${ page }&${ PARAM_HPP }${ DEFAULT_HPP }`)
             .then(response => response.json())
             .then(result => this.setSearchTopStories(result))
-            .catch(error => error);
+            .catch(error => this.setState({ error }));
     }
 
     componentDidMount() {
@@ -139,7 +140,7 @@ class App extends Component {
 
   render() {
 
-    const { searchTerm, results, searchKey } = this.state;
+    const { searchTerm, results, searchKey, error } = this.state;
     //console.log(this.state);
 
     const page = (
@@ -165,10 +166,16 @@ class App extends Component {
                   Поиск
               </Search>
           </div>
-          <Table
-              list={ list }
-              onDismiss={ this.onDismiss }
-          />
+          { error
+              ? <div className="interactions">
+                  <p>Что-то пошло не так...</p>
+              </div>
+              :
+              <Table
+                  list={ list }
+                  onDismiss={ this.onDismiss }
+              />
+          }
           <div className="interactions">
               <Button onClick={ () => this.fetchSearchTopStories(searchKey, page + 1) }>
                   Больше историй
